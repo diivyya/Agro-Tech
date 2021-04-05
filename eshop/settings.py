824c030c81c015f06eeb4acd8d8bb293e4e1e38f
Divya Tembhurne <68610804+diivyya@users.noreply.github.com>
2021-04-06 02:25:33 +0530

    Update settings.py

--- eshop/settings.py
+++ eshop/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-'''
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
     # Add to this list all the locations containing your static files 
 )
-'''
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
