1a7f61cb208f153dbfcab96354aa6dad47831aaf
Divya Tembhurne <68610804+diivyya@users.noreply.github.com>
2021-04-06 02:04:39 +0530

    Update wsgi.py

--- eshop/wsgi.py
+++ eshop/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eshop.settings')
 
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
