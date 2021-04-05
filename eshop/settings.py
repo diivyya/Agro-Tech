2fc68ef7732ee0261709333b6dd90f9d07db7a2a
Divya Tembhurne <divyatembhurne1107@gmail.com>
2021-04-06 01:36:33 +0530

    .

--- eshop/settings.py
+++ eshop/settings.py
@@ -17,6 +17,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import django_heroku
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
